229c1700cf31875de3f889d9e181fef147e87852
naxxraz <44828850+Naxxraz@users.noreply.github.com>
2025-02-11 09:35:57 +0100

    Test

---
+++ Contact/model.js
@@ -0,0 +1,16 @@
+//model.js
+
+const app = document.getElementById('main');
+
+const model = {
+    app: {
+        currentPage: "NewsletterPage",
+        theme: "dark",
+    },
+    input: {
+
+    },
+    data: {
+
+    }
+}
